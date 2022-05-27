b22c664dcc281538665ed78f95860ca862f0537b
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-05-27 10:19:17 +0530

    Created basic CRUD operations

---
+++ main.py
@@ -0,0 +1,110 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+my_posts = [
+    {
+        "title": "This is her 1st quote.",
+        "content": "Beauty comes from within.",
+        "id": 1
+    },
+    {
+        "title": "This is her 2nd quote.",
+        "content": "If not me, who? If not now, when?",
+        "id": 2
+    },
+    {
+        "title": "This is her 3rd quote.",
+        "content": "Girls should never be afraid to be smart.",
+        "id": 3
+    },
+    {
+        "title": "This is her 4th quote.",
+        "content": "The less you reveal, the more people can wonder.",
+        "id": 4
+    },
+    {
+        "title": "This the her 5th quote.",
+        "content": "Feeling beautiful has nothing to do with what you look like, I promise.",
+        "id": 5
+    }
+
+]
+
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == int(id):
+            return post
+
+
+def get_post_index(id):
+    for index, post in enumerate(my_posts):
+        if post['id'] == id:
+            return index
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+async def posts():
+    return {"posts": my_posts}
+
+
+@app.get('/about')
+async def about():
+    return {"about": "This is the about page."}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+async def create_posts(post: Post):
+    posts_dict = post.dict()
+    posts_dict['id'] = randrange(0, 100000)
+    my_posts.append(posts_dict)
+    return {"data": posts_dict}
+
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"A quote with the id: {id} was not found.")
+    return {"message": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    index = get_post_index(id)
+    if index is not None:
+        my_posts.pop(index)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"A quote with the id: {id} was not found.")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+async def update_post(id: int, post: Post):
+    index = get_post_index(id)
+    if index is not None:
+        post_dict = post.dict()
+        post_dict["id"] = id
+        my_posts[index] = post_dict
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"A quote with the id: {id} was not found.")
+    return {"message": post_dict}
